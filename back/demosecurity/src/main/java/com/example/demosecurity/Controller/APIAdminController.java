7e1873370f8ee7177d966de3713efc8b5940f046
Nguyen Sy Manh <nsmanh.15@gmail.com>
2021-02-26 18:30:30 +0700

    manh update

--- back/demosecurity/src/main/java/com/example/demosecurity/Controller/APIAdminController.java
+++ back/demosecurity/src/main/java/com/example/demosecurity/Controller/APIAdminController.java
@@ -6,7 +6,7 @@ import com.example.demosecurity.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/secure/auth/")
 public class APIAdminController {

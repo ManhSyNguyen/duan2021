ae5df1d494290ea53edf191ff626a42b5d962bc8
nddduong2000 <ndduong2000@gmail.com>
2021-04-15 00:47:38 +0700

    Update

--- cms-dhm/src/app/app.module.ts
+++ cms-dhm/src/app/app.module.ts
@@ -36,17 +36,12 @@ import { OdersComponent } from './cmsdhm/oder/oders/oders.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { authInterceptorProviders } from './interceptor/basic-auth-intercepter';
 import { ErrorInterceptor } from './interceptor/error.interceptor';
-<<<<<<< HEAD
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { ToastrModule } from 'ngx-toastr';
-=======
 import { DetailOderComponent } from './cmsdhm/oder/detail-oder/detail-oder.component';
 import { AddBillComponent } from './cms-member/add-bill/add-bill.component';
 import { DetailBillComponent } from './cms-member/detail-bill/detail-bill.component';
 
-
-
->>>>>>> manh
 
 @NgModule({
   declarations: [
